043e049be7b8c9fc68755fe48cec982b3b2520b2
dlrjs2360 <lhg3670@naver.com>
2023-01-09 22:21:21 +0900

    :sparkles: feat: Springboot 초기 세팅
    설치된 라이브러리
    - lombok
    - Springboot Configuration
    - Springboot Thymeleaf
    - Springboot Web

---
+++ src/main/java/com/zandihub/zandihub/ZandiHubApplication.java
@@ -0,0 +1,13 @@
+package com.zandihub.zandihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZandiHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZandiHubApplication.class, args);
+    }
+
+}
