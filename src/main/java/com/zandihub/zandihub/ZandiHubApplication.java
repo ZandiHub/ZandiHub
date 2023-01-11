61e49b1f8d1f600355741c81a8d49745e1ba7b62
dlrjs2360 <lhg3670@naver.com>
2023-01-12 00:08:28 +0900

    :sparkles: feat: toast-ui 적용
    1. toast-ui 설치 (필수 x)
    npm install --save @toast-ui/editor
    
    2. 또는 CDN으로 설치 (<body>안에 toast에디터 아래에 두기)
    <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js"></script>
    
    3. 적용완료!

--- src/main/java/com/zandihub/zandihub/ZandiHubApplication.java
+++ src/main/java/com/zandihub/zandihub/ZandiHubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZandiHubApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZandiHubApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZandiHubApplication.class, args);
+	}
 
 }
