eb1705faf468d59332dd2cec0386a21b86b21b1d
dlrjs2360 <lhg3670@naver.com>
2023-01-21 13:36:12 +0900

    :bug: fix: 주기능 레이아웃 미완성
    TO-DO
    - 아이템박스, 아이템설명박스 비율조절 필요
    - 에디터 반응형 필요
    - 상단바 꾸밈 필요

--- src/main/resources/templates/js/toast-ui.js
+++ src/main/resources/templates/js/toast-ui.js
@@ -1,12 +1,12 @@
 /*시스템 설정에 따라 다크모드 전환 가능*/
-const isBrowserDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
-    .matches
+const isBrowserDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
 let initTheme = isBrowserDarkMode ? 'dark' : 'light'
 
 const editor = new toastui.Editor({
     el: document.querySelector('#editor'),
     previewStyle: 'vertical',
-    height: '1300px',
+    height: 'auto',
+    minHeight: '600px',
     initialValue: '',
-    theme: initTheme
+    theme: initTheme,
 });
